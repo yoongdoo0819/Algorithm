bbd058cdf809a04ea448e58a575339f9fd4b7fca
YoongdooNoh <yoongdoo0819@naver.com>
2021-11-14 00:09:56 +0900

    book

--- 미로_탈출.py
+++ 미로_탈출.py
@@ -1,11 +1,13 @@
+# 미로 탈출
+
 from collections import deque
 
 n, m = map(int, input().split())
-
 graph = []
-for i in range(n):
-    graph.append(list(map(int, input())))
 
+for i in range(n):
+    graph.append(list(map(int, input().split())))
+    
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
@@ -17,25 +19,18 @@ def bfs(x, y):
     while queue:
         x, y = queue.popleft()
         
+        if x == n-1 and y == m-1:
+            break
+        
         for i in range(4):
-            nx = x + dx[i] 
+            nx = x + dx[i]
             ny = y + dy[i]
             
             if nx < 0 or nx >= n or ny < 0 or ny >= m:
                 continue
-            if graph[nx][ny] == 0:
-                continue
-            
-            # 첫 번째 (0,0) 위치가 다시 방문되어도 값이 1로 유지되도록 하기 위해 
-            # (본 문제에서는 사실상 제거해도 무방)
-            if nx == 0 and ny == 0:
-                continue
-            
             if graph[nx][ny] == 1:
                 graph[nx][ny] += graph[x][y]
                 queue.append((nx, ny))
-                
-    return graph[n-1][m-1]
-
-print(bfs(0, 0))
-print(graph)
+            
+bfs(0, 0)
+print(graph[n-1][m-1])
