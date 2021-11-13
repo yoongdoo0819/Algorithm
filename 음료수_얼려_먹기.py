ba619fd16c41ec1ade2225e7ea6837a7205a5b23
YoongdooNoh <yoongdoo0819@naver.com>
2021-11-13 23:51:12 +0900

    book

--- 음료수_얼려_먹기.py
+++ 음료수_얼려_먹기.py
@@ -1,30 +1,34 @@
-n, m = map(int, input().split())
 
+# 음료수 얼려 먹기
+
+n, m = map(int, input().split())
 graph = []
 for i in range(n):
-    graph.append(list(map(int, input())))
+    graph.append(list(map(int, input().split())))
+    
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
 
 def dfs(x, y):
     
-    if x <= -1 or x >= n or y <= -1 or y >= m:
-        return False
-    
-    if graph[x][y] == 0:
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
         
-        graph[x][y] = 1
-        
-        dfs(x-1, y)
-        dfs(x+1, y)
-        dfs(x, y-1)
-        dfs(x, y+1)
-        return True
-    return False
+        if nx < 0 or nx >= n or ny < 0 or ny >= m:
+            continue
+        if graph[nx][ny] == 0:
+            graph[nx][ny] = 1
+            dfs(nx, ny)
+            
+    return True
 
-result = 0
+cnt = 0
 for i in range(n):
     for j in range(m):
-        
-        if dfs(i, j) == True:
-            result +=1
-            
-print(result)
+        if graph[i][j] == 0:
+            graph[i][j] = 1
+            if dfs(i, j) == True:
+                cnt += 1
+                
+print(cnt)
