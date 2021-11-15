609a57882a4aa13869df3cc040f6d4a6af2a65f6
YoongdooNoh <yoongdoo0819@naver.com>
2021-11-16 05:41:00 +0900

    볼링공 고르기

---
+++ book17.py
@@ -0,0 +1,26 @@
+# 볼링공 고르기
+
+from itertools import combinations
+
+n, m = map(int, input().split())
+datas = list(map(int, input().split()))
+cnt = 0
+
+for data in combinations(datas, 2):
+    print(data)
+    if data[0] != data[1]:
+        cnt += 1
+    
+print(cnt)
+"""
+array = [0] * 11
+for x in datas:
+    array[x] += 1
+    
+result = 0
+for i in range(1, m+1):
+    n -= array[i]
+    result += array[i] * n
+    
+print(result)
+"""
