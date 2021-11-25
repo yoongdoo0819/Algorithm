bc4d60466bd4dfd60f5a3fe96f8b22b392cb2f0b
YoongdooNoh <yoongdoo0819@naver.com>
2021-11-26 03:05:47 +0900

    최솟값 만들기

---
+++ programmers29.py
@@ -0,0 +1,12 @@
+# 최솟값 만들기
+
+def solution(A,B):
+    answer = 0
+    
+    A.sort()
+    B.sort(reverse=True)
+    
+    for idx in range(len(A)):
+        answer += (A[idx] * B[idx])
+        
+    return answer
