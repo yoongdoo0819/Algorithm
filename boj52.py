c7c8c8d518d50839a1e0785424c47a7c371cb203
YoongdooNoh <yoongdoo0819@naver.com>
2021-12-08 22:10:14 +0900

    상어 초등학교 (BOJ 21608)

--- boj52.py
+++ boj52.py
@@ -1,4 +1,6 @@
-# 상어 초등학교 (BOJ 21608) (못품)
+# 상어 초등학교 (BOJ 21608) 
+
+from copy import deepcopy
 
 n = int(input())
 
@@ -9,10 +11,17 @@ for _ in range(n*n):
     num, a, b, c, d = map(int, input().split())
     friends_list.append((num, a, b, c, d))
     
+def max_xy():
+    for x in range(n):
+        for y in range(n):
+            if graph[x][y] == 0:
+                return x, y
+                
 def arrange_seat(std, a, b, c, d):
-    friend_seat_graph = [ [0] * n for _ in range(n) ]
-    empty_seat_graph = [ [0] * n for _ in range(n) ]
-    check = False
+    friend_seat_graph =  [ [0] * n for _ in range(n) ]
+    empty_seat_graph =  [ [0] * n for _ in range(n) ]
+    friend_check = False
+    empty_check = False
     
     for x in range(n):
         for y in range(n):
@@ -26,39 +35,46 @@ def arrange_seat(std, a, b, c, d):
                         continue
                     if graph[nx][ny] in [a, b, c, d]:
                         friend_seat_graph[x][y] += 1
-                        check = True
+                        friend_check = True
                     if graph[nx][ny] == 0:
                         empty_seat_graph[x][y] += 1
+                        empty_check = True
                         
-    max_x, max_y = -1, -1
-    if check == True:
+    max_x, max_y = 0, 0
+    # 인접한 곳이 자리가 꽉 차있으면서, 좋아하는 친구가 자리를 앉은 것도 아니라면
+    if friend_check == False and empty_check == False:
+        max_x, max_y = max_xy()
+                    
+    # 인접한 곳 중 좋아하는 친구가 있다면, 
+    elif friend_check == True:
         max = -1e9
         max_val_cnt = 0
+        
+        # 좋아하는 친구가 가장 많이 인접한 자리에 착석
         for x in range(n):
             for y in range(n):
                 if max < friend_seat_graph[x][y]:
                     max = friend_seat_graph[x][y]
+                    max_x, max_y = x, y
         
+        # 좋아하는 친구가 가장 많이 인접한 자리가 한 개인지 확인하고,
         for x in range(n):
             for y in range(n):
                 if max == friend_seat_graph[x][y]:
                     max_val_cnt += 1
         
+        # 좋아하는 친구가 가장 많이 인접한 자리가 두 개 이상이라면, 
+        # 좋아하는 친구가 가장 많이 인접한 자리 중 주변에 비어있는 자리가 최대한 많은 곳에 착석 
         if max_val_cnt > 1:
             empty_max_val = -1e9
             for x in range(n):
                 for y in range(n):
                     if friend_seat_graph[x][y] == max and empty_max_val < empty_seat_graph[x][y]:
                         empty_max_val = empty_seat_graph[x][y]
-                        max_x, max_y = x, y
-        elif max_val_cnt == 1:
-            for x in range(n):
-                for y in range(n):
-                    if friend_seat_graph[x][y] == max:
-                        max_x, max_y = x, y
-                        
-                        
-    elif check == False:
+                        max_x, max_y = x, y         
+    
+    # 인접한 곳 중 좋아하는 친구는 없고 자리가 비어있다면, 주변에 비어있는 자리가 최대한 많은 곳에 착석     
+    elif friend_check == False:
         empty_max_val = -1e9
         for x in range(n):
             for y in range(n):
@@ -68,6 +84,7 @@ def arrange_seat(std, a, b, c, d):
                     
     graph[max_x][max_y] = std
     
+        
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 for friend in friends_list:
