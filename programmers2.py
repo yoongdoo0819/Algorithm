74929f9aa9980caf341129b30c81b5d8966a85fd
YoongdooNoh <yoongdoo0819@naver.com>
2021-12-11 01:09:39 +0900

    타겟 넘버

--- programmers2.py
+++ programmers2.py
@@ -1,19 +1,51 @@
 # 타겟 넘버
 
-cnt = 0
+"""
+# 방법 1
+from itertools import product
+def solution(numbers, target):
+    l = [(x, -x) for x in numbers]
+    print(l)
+    s = list(map(sum, product(*l)))
+    print(s)
+    return s.count(target)
+"""
 
-def dfs(numbers, target, depth, num_sum):
-    global cnt
+"""
+# 방법 2
+cnt = 0
+def dfs(numbers, idx, sum_val, target):
+    global cnt 
     
-    if len(numbers) == depth:
-        if num_sum == target:
+    if idx >= len(numbers):
+        if sum_val == target:
             cnt += 1
         return
     
-    dfs(numbers, target, depth+1, num_sum + numbers[depth])
-    dfs(numbers, target, depth+1, num_sum - numbers[depth])
+    dfs(numbers, idx+1, sum_val+numbers[idx], target)
+    dfs(numbers, idx+1, sum_val-numbers[idx], target)
     
 def solution(numbers, target):
     
-    dfs(numbers, target, 0, 0)
+    dfs(numbers, 0, 0, target)
+    return cnt
+"""
+
+# 방법 3
+from collections import deque
+def solution(numbers, target):
+    cnt = 0
+    q = deque()
+    q.append((+numbers[0], 1))
+    q.append((-numbers[0], 1))
+    
+    while q:
+        val, idx = q.popleft()
+        if idx >= len(numbers):
+            if val == target:
+                cnt += 1
+        else:
+            q.append((val+numbers[idx], idx+1))
+            q.append((val-numbers[idx], idx+1))
+            
     return cnt
