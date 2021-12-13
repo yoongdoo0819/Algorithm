cbe96fa69e116041710a4e4b6f018502c585b480
YoongdooNoh <yoongdoo0819@naver.com>
2021-12-13 17:06:38 +0900

    N과 M (2) (BOJ 15650)

--- boj61.py
+++ boj61.py
@@ -1,5 +1,7 @@
 # N과 M (2) (BOJ 15650)
 
+"""
+# 해결 방안 1
 from itertools import combinations
 
 n, m = map(int, input().split())
@@ -12,3 +14,25 @@ for comb_list in combinations(data, m):
     for val in comb_list:
         print(val, end = ' ')
     print("")
+"""
+
+""" 해결 방안 2 """
+n, m = map(int, input().split())
+s = []
+
+def dfs(i):
+    
+    #print("curr", s, i)
+    if len(s) == m:
+        print(' '.join(map(str, s)))
+        #print("exit", i)
+        return
+    else:
+        for val in range(i, n+1):
+            if not val in s:
+                #print("add", val, " before ", val+1)
+                s.append(val)
+                dfs(val+1)
+                s.pop()
+                #print("pop", val, " after", val+1)
+dfs(1)
