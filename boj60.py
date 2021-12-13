7ebbf446c3e07600c1c5914d9f498dd1389c1c0c
YoongdooNoh <yoongdoo0819@naver.com>
2021-12-13 15:45:11 +0900

    N과 M (1) (BOJ 15649)

--- boj60.py
+++ boj60.py
@@ -1,5 +1,8 @@
 # N과 M (1) (BOJ 15649)
 
+""" 
+# 해결 방안 1
+
 from itertools import permutations
 n, m = map(int, input().split())
 
@@ -11,3 +14,22 @@ for val in permutations(permutation_list, m):
     for i in range(m):
         print(val[i], end =' ')
     print("")
+"""
+
+""" 해결 방안 2 """
+n, m = map(int, input().split())
+s = []
+
+def dfs():
+    
+    if len(s) == m:
+        print(' '.join(map(str, s)))
+        return
+    
+    for i in range(1, n+1):
+        if not i in s:
+            s.append(i)
+            dfs()
+            s.pop()
+
+dfs()
