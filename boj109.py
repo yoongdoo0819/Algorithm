d15e3f6b8e4fa20cdf18cf00ff789db068f793d3
YoongdooNoh <yoongdoo0819@naver.com>
2022-01-20 04:13:23 +0900

    에너지 드링크 (20115)

---
+++ boj109.py
@@ -0,0 +1,12 @@
+# 에너지 드링크 (20115)
+
+n = int(input())
+energy_drink = list(map(int, input().split()))
+
+energy_drink.sort(reverse=True)
+sum = energy_drink[0]
+
+for i in range(1, n):
+    sum += energy_drink[i]/2
+    
+print(sum)
