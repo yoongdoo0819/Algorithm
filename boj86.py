a3408625709bfa7e02e08223ef2ca58ce44bcaa1
YoongdooNoh <yoongdoo0819@naver.com>
2022-01-26 03:31:28 +0900

    Four Squares (BOJ 17626)

--- boj86.py
+++ boj86.py
@@ -1,23 +1,24 @@
 # Four Squares (BOJ 17626)
 
+from math import sqrt
 n = int(input())
 
 def find_four_squares():
-    square_root = n**0.5
+    square_root = sqrt(n) # sqrt(n) == (n ** 0.5)
     # if n == a * a:
     #     return 1
-    if (n ** 0.5).is_integer():
+    if sqrt(n).is_integer():
         return 1
     
     for i in range(1, int(square_root)+1):
         if i**2 > n:
             break
-        if ((n-i**2)**0.5).is_integer():
+        if sqrt(n-i**2).is_integer():
             return 2
     for i in range(1, int(square_root)+1):
         if i**2 > n:
             break
-        for j in range(1, int((n-i**2)**0.5)+1):
+        for j in range(1, int(sqrt(n-i**2))+1):
             if i**2 + j**2 > n:
                 break
             #if ((n - i ** 2 - j ** 2) ** 0.5).is_integer():
@@ -27,10 +28,37 @@ def find_four_squares():
                 # print((n - i ** 2 - j ** 2))
                 # print((n - i ** 2 - j ** 2) ** 0.5)
                 #return 3
-            if ((n-(i**2 + j**2))**0.5).is_integer():
+            if (sqrt(n-(i**2 + j**2))).is_integer():
                 #print((n-(i**2 + j**2)**0.5))
                 return 3
             
     return 4
 
 print(find_four_squares())
+
+"""
+아래 코드는 시간과 메모리가 충분하다면 동작할런지 ?
+
+from math import sqrt
+import sys
+
+sys.setrecursionlimit(10 ** 6)
+N = int(input())
+
+ans = 1e9
+def dfs(sum, cnt):
+    global ans
+    
+    if sum == N:
+        ans = min(ans, cnt)
+        return
+    elif sum > N:
+        return
+    
+    for i in range(1, int(sqrt(N)+1)):
+        dfs(sum + (i ** 2), cnt + 1)
+        
+dfs(0, 0)
+print(ans)
+
+"""
