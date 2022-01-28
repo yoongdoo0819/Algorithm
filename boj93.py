970c6ed477a480dc86a09a632656340ff8208ba9
YoongdooNoh <yoongdoo0819@naver.com>
2022-01-28 16:06:50 +0900

    스타트와 링크 (BOJ 14889)

--- boj93.py
+++ boj93.py
@@ -1,5 +1,52 @@
 # 스타트와 링크 (BOJ 14889)
 
+"""
+dfs로 해결
+
+"""
+N = int(input())
+teams = []
+ans = 1e9
+
+for _ in range(N):
+    teams.append(list(map(int, input().split())))
+
+def dfs(start_team, idx, cnt):
+    global ans
+    
+    if cnt == N//2:
+        link_team = [i for i in range(N)]
+        for i in start_team:
+            link_team.remove(i)
+        
+        start_team_ability = 0
+        for i in start_team:
+            for j in start_team:
+                start_team_ability += teams[i][j]
+                
+        
+        link_team_ability = 0
+        for i in link_team:
+            for j in link_team:
+                link_team_ability += teams[i][j]
+                
+        ans = min(ans, abs(start_team_ability-link_team_ability))
+        return
+        
+    for i in range(idx, N):
+        start_team.append(i)
+        dfs(start_team, i+1, cnt+1)
+        start_team.pop(-1)
+        
+dfs([], 0, 0)
+print(ans)
+
+"""
+아래 코드들은 combinations을 활용하여 해결한 두 가지 방안
+(코드는 약간 다름)
+
+
+첫 번째 해결 코드. 
 from itertools import combinations
 
 n = int(input())
@@ -28,9 +75,8 @@ for member in members:
 
 print(min_diff)
 
-"""
-아래 코드는 combination을 사용하여 해결
 
+두 번째 해결 코드.
 from itertools import combinations
 
 N = int(input())
