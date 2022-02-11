8e8f45e0106eb365b83dce2185ebc826f8f55597
YoongdooNoh <yoongdoo0819@naver.com>
2022-02-11 20:35:49 +0900

    샘터 (BOJ 18513)

--- boj25.py
+++ boj25.py
@@ -39,3 +39,44 @@ def bfs():
                     return
 
 bfs()
+
+"""
+set() 대신 {}를 사용해도 통과.
+
+from collections import deque
+
+N, K = map(int, input().split())
+sam_arr = list(map(int, input().split()))
+q = deque()
+
+for sam in sam_arr:
+    q.append(sam)
+
+visited = {}
+for sam in sam_arr:
+    visited[sam] = True
+    
+def bfs():
+    ans, cnt = 0, 0
+    idx = 1
+    
+    while True:
+
+        for _ in range(len(q)):
+            s = q.popleft()
+            
+            for next_sam in [s-1, s+1]:
+                if visited.get(next_sam) == None:
+                    visited[next_sam] = True
+                    ans += idx 
+                    cnt += 1
+                    q.append(next_sam)
+                    
+                    if cnt == K:
+                        return ans
+        idx += 1            
+        
+ans = bfs()
+print(ans)
+
+"""
