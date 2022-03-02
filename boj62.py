b1f209a189a6cb7b7058ec1c7d780384fb3b31ff
YoongdooNoh <yoongdoo0819@naver.com>
2022-03-02 20:07:47 +0900

    N과 M (3) (BOJ 15651)

--- boj62.py
+++ boj62.py
@@ -19,3 +19,29 @@ def dfs():
         s.pop()
         
 dfs()
+
+"""
+직접 짠 코드
+
+N, M = map(int, input().split())
+
+def dfs(res, cnt):
+    
+    if cnt == M:
+        for i in res:
+            print(i, end=" ")
+        print("")
+        return
+    
+    for i in range(1, N+1):
+        res.append(i)
+        dfs(res, cnt + 1)
+        res.pop(-1)
+
+res = []        
+for i in range(1, N+1):
+    res.append(i)
+    dfs(res, 1)
+    res.pop(-1)
+
+"""
