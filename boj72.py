0fa588462ce1ad5502de3228c089583c0a1e1d6a
YoongdooNoh <yoongdoo0819@naver.com>
2022-03-03 21:30:30 +0900

    부분수열의 합 (BOJ 1182)

--- boj72.py
+++ boj72.py
@@ -24,3 +24,32 @@ def dfs(start, result):
 
 dfs(0, result)
 print(cnt)
+
+"""
+다시 짠 코드
+
+N, M = map(int, input().split())
+datas = list(map(int, input().split()))
+ans = 0
+
+def dfs(start, res, cnt):
+    global ans
+    
+    if sum(res) == M and len(res) > 0:
+        #print(">>", res)
+        ans += 1
+
+    # 아래 if문은 없어도 정답처리 됨.
+    if cnt >= N:
+        return
+    
+    for i in range(start, N):
+        data = datas[i]
+        res.append(data)
+        dfs(i+1, res, cnt + 1)
+        res.pop(-1)
+
+dfs(0, [], 0)
+print(ans)
+
+"""
