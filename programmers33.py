59fb61397e062f41c7975d3f8a39b3bb1c45d9e0
YoongdooNoh <yoongdoo0819@naver.com>
2022-03-06 13:54:01 +0900

    단어 변환

--- programmers33.py
+++ programmers33.py
@@ -32,3 +32,39 @@ def solution(begin, target, words):
         
     bfs(begin, target, words, visited)
     return visited[target]
+
+
+"""
+아래 코드는 위 코드처럼 visited 변수를 안 쓰는 대신, 각 단어마다 cnt를 붙여서 해결
+
+from collections import deque
+
+def bfs(begin, target, words):
+    q = deque()
+    q.append((begin, 0))
+    
+    while q:
+        curr_word, cnt = q.popleft()
+        
+        if curr_word == target:
+            return cnt
+        
+        for word in words:
+            if curr_word == word:
+                continue
+            
+            temp_cnt = 0
+            for idx in range(len(curr_word)):
+                if curr_word[idx] != word[idx]:
+                    temp_cnt += 1
+            
+            if temp_cnt == 1 :
+                q.append((word, cnt+1))
+    return 0
+
+def solution(begin, target, words):
+    
+    answer = bfs(begin, target, words)
+    return answer
+
+"""
